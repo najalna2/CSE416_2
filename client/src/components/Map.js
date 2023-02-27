7c9b2315a8e8f8298af000fcbe3916091eed5acc
araguma <jia.lin@stonybrook.edu>
2023-02-27 10:27:40 -0500

    Highlight state boundaries

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,22 +1,32 @@
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { MapContainer, TileLayer, GeoJSON } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 
 import React from 'react';
 import '../css/Map.css';
 
-const position = [51.505, -0.09];
+import arGeoJSON from '../data/ar.json';
+import flGeoJSON from '../data/fl.json';
+import msGeoJSON from '../data/ms.json';
 
 export default class Map extends React.Component {
     constructor(props) {
         super(props);
+        this.mapStyle = {
+            color: 'white',
+            fillColor: 'red',
+            weight: 1,
+        }
     }
     render() {
         return (
-            <MapContainer center={position} zoom={13}>
+            <MapContainer center={[31.5256333, -87.7335333]} zoom={6}>
                 <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
                 />
+                <GeoJSON style={this.mapStyle} data={arGeoJSON.features}></GeoJSON>
+                <GeoJSON style={this.mapStyle} data={flGeoJSON.features}></GeoJSON>
+                <GeoJSON style={this.mapStyle} data={msGeoJSON.features}></GeoJSON>
             </MapContainer>
         )
     }
